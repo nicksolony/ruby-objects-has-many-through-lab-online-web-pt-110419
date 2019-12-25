5552e3acf93850353691dd9a340821b960073835
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 20:01:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,8 +17,8 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
-  def new_appointment(patient, date)
-    Appointment.new(patient,self,date)
+  def new_appointment(date, patient)
+    Appointment.new(date,self,patient)
   end
   
     def patients
