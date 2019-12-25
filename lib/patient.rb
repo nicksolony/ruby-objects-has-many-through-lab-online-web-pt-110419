9e1b8ed56f5792c6fcff924d4da278c4910a0112
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 20:06:22 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,6 +11,12 @@ class Patient
     @@all
   end
   
+   def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+ 
+  
+  
   def new_appointment(doctor,date)
   #  binding.pry
     Appointment.new(date,self,doctor)
