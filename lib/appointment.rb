ac2d939519c43ee4605bb0eeab981aa590c7a9f7
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 19:58:24 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
  attr_accessor :patient, :doctor,:date
   @@all = []
   
-  def initialize (date, patient, doctor)
+  def initialize (patient, doctor,date)
     @date = date
     @patient = patient
     @doctor = doctor
