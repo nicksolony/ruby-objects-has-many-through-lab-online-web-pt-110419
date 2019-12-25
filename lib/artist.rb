0271c3ad14fb2be563f5108ee212b062ca01ed75
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 19:33:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,6 +17,10 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
   
+  def genres
+    songs.collect {|song| song.genre}
+  end
+  
   def new_song(name,genre)
     Song.new(name,self,genre)
   end
