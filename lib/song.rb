4efda0d6e69846c25ba9f6c15095098550d90735
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 19:27:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,11 @@
 class Song
-  attr_reader :name
+  attr_reader :name, :artist, :genre
+  @@all = []
   
-
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
 end
